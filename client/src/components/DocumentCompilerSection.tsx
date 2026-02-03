f32f38a44eae4bad0e8fa73ecb56061ccb964a15
Carlo Galli <carlogalli@192.168.1.107>
2026-02-03 09:56:09 +0100

    fix: robust regex for multi-escaped brackets

--- client/src/components/DocumentCompilerSection.tsx
+++ client/src/components/DocumentCompilerSection.tsx
@@ -370,10 +370,10 @@ export function DocumentCompilerSection({
 
       const data = await response.json();
       if (data.compiledContent) {
-        // Sanitize escaped brackets (Standard Regex)
+        // Sanitize escaped brackets (Robust Regex for multiple escapes)
         let sanitizedContent = data.compiledContent
-          .replace(/\\\[/g, '[')
-          .replace(/\\\]/g, ']');
+          .replace(/\\+\[/g, '[')
+          .replace(/\\+\]/g, ']');
 
         // Force checkboxes to be list items for Tiptap (replace "^[ ]" with "- [ ]")
         sanitizedContent = sanitizedContent.replace(/^(\s*)\[([ xX])\]/gm, '$1- [$2]');
